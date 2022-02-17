c38fc01593884527c3f616cfcd49511c1fc348c1
Morazan <morazan@pop-os.localdomain>
2022-02-17 13:08:44 -0600

    deploy heroku

---
+++ app/models/bucket.rb
@@ -0,0 +1,28 @@
+class Bucket < ApplicationRecord
+    # belongs_to :bucket bucket:references resource has_many :tasks
+
+    has_many :tasks
+
+    def self.get_by_status(status)
+        self.all.select do |bucket|
+            bucket.status == status
+        end
+    end
+
+    def update_status
+        if self.tasks == []
+            self.status = "Empty"
+        elsif self.tasks.any?{|task| task.status == "Pending"}
+            self.status = "Pending"
+        else
+            self.status = "Completed"
+        end
+        self.save
+    end
+
+    def tasks_by_status(status)
+        self.tasks.select do |task|
+            task.status == status
+        end
+    end
+end
